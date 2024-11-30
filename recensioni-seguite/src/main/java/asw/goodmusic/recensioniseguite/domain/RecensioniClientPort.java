3dfabd0e1ccb95173f25aa878146c23a5092f2ed
Missi5 <74096133+Missi5@users.noreply.github.com>
2024-11-30 14:17:24 +0100

    prima modifica (obbligatoria)

--- recensioni-seguite/src/main/java/asw/goodmusic/recensioniseguite/domain/RecensioniClientPort.java
+++ recensioni-seguite/src/main/java/asw/goodmusic/recensioniseguite/domain/RecensioniClientPort.java
@@ -1,11 +1,13 @@
 package asw.goodmusic.recensioniseguite.domain;
 
-import java.util.*; 
+import java.util.*;
 
 public interface RecensioniClientPort {
 
-	public Collection<RecensioneBreve> getRecensioniByArtisti(Collection<String> artisti); 
+    public Collection<RecensioneBreve> getRecensioniByArtisti(Collection<String> artisti);
 
-	public Collection<RecensioneBreve> getRecensioniByRecensori(Collection<String> recensori); 
+    public Collection<RecensioneBreve> getRecensioniByRecensori(Collection<String> recensori);
+
+    public Collection<RecensioneBreve> getRecensioniByGeneri(Collection<String> generi);
 
 }
--- recensioni-seguite/src/main/java/asw/goodmusic/recensioniseguite/recensioniclient/RecensioniRestClientAdapter.java
+++ recensioni-seguite/src/main/java/asw/goodmusic/recensioniseguite/recensioniclient/RecensioniRestClientAdapter.java
@@ -1,8 +1,8 @@
 package asw.goodmusic.recensioniseguite.enigmi;
 
-import asw.goodmusic.recensioniseguite.domain.*; 
+import asw.goodmusic.recensioniseguite.domain.*;
 
-import asw.goodmusic.recensioni.api.rest.*; 
+import asw.goodmusic.recensioni.api.rest.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,22 +14,22 @@ import org.springframework.web.reactive.function.client.WebClientException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.*; 
-import java.util.stream.*; 
+import java.util.*;
+import java.util.stream.*;
 
-@Service 
-@Primary 
+@Service
+@Primary
 public class RecensioniRestClientAdapter implements RecensioniClientPort {
 
-	@Autowired 
-	@Qualifier("loadBalancedWebClient")
+    @Autowired
+    @Qualifier("loadBalancedWebClient")
     private WebClient loadBalancedWebClient;
-	
-	public Collection<RecensioneBreve> getRecensioniByArtisti(Collection<String> artisti) {
-		Collection<RecensioneBreve> recensioni = null; 
+
+    public Collection<RecensioneBreve> getRecensioniByArtisti(Collection<String> artisti) {
+        Collection<RecensioneBreve> recensioni = null;
         Flux<RecensioneBreveResponse> response = loadBalancedWebClient
                 .get()
-				.uri("http://recensioni/cercarecensioni/artisti/{artisti}", toString(artisti))
+                .uri("http://recensioni/cercarecensioni/artisti/{artisti}", toString(artisti))
                 .retrieve()
                 .bodyToFlux(RecensioneBreveResponse.class);
         try {
@@ -37,14 +37,14 @@ public class RecensioniRestClientAdapter implements RecensioniClientPort {
         } catch (WebClientException e) {
             e.printStackTrace();
         }
-		return recensioni; 
-	}	
+        return recensioni;
+    }
 
-	public Collection<RecensioneBreve> getRecensioniByRecensori(Collection<String> recensori) {
-		Collection<RecensioneBreve> recensioni = null; 
+    public Collection<RecensioneBreve> getRecensioniByRecensori(Collection<String> recensori) {
+        Collection<RecensioneBreve> recensioni = null;
         Flux<RecensioneBreveResponse> response = loadBalancedWebClient
                 .get()
-				.uri("http://recensioni/cercarecensioni/recensori/{recensori}", toString(recensori))
+                .uri("http://recensioni/cercarecensioni/recensori/{recensori}", toString(recensori))
                 .retrieve()
                 .bodyToFlux(RecensioneBreveResponse.class);
         try {
@@ -52,29 +52,45 @@ public class RecensioniRestClientAdapter implements RecensioniClientPort {
         } catch (WebClientException e) {
             e.printStackTrace();
         }
-		return recensioni; 
-	}
+        return recensioni;
+    }
 
-	private RecensioneBreve toRecensione(RecensioneBreveResponse response) {
-		return new RecensioneBreve(response.getId(), response.getRecensore(), response.getAlbum(), 
-				response.getArtista(), response.getGenere(), response.getSunto());
-	}
+    @Override
+    public Collection<RecensioneBreve> getRecensioniByGeneri(Collection<String> generi) {
+        Collection<RecensioneBreve> recensioni = null;
+        Flux<RecensioneBreveResponse> response = loadBalancedWebClient
+                .get()
+                .uri("http://cercarecensioni/generi/{insieme-di-generi}", toString(generi))
+                .retrieve()
+                .bodyToFlux(RecensioneBreveResponse.class);
+        try {
+            recensioni = toRecensioni(response.collectList().block());
+        } catch (WebClientException e) {
+            e.printStackTrace();
+        }
+        return recensioni;
+    }
 
-	private Collection<RecensioneBreve> toRecensioni(Collection<RecensioneBreveResponse> response) {
-		Collection<RecensioneBreve> recensioni = 
-			response
-				.stream()
-				.map(r -> toRecensione(r))
-				.collect(Collectors.toList());
-		return recensioni; 
-	}
+    private RecensioneBreve toRecensione(RecensioneBreveResponse response) {
+        return new RecensioneBreve(response.getId(), response.getRecensore(), response.getAlbum(),
+                response.getArtista(), response.getGenere(), response.getSunto());
+    }
 
-	private static String toString(Collection<String> c) {
-		String result = 
-			c.stream()
-				.map(n -> String.valueOf(n))
-				.collect(Collectors.joining(",", "", ""));
-		return result; 
-	}
+    private Collection<RecensioneBreve> toRecensioni(Collection<RecensioneBreveResponse> response) {
+        Collection<RecensioneBreve> recensioni =
+                response
+                        .stream()
+                        .map(r -> toRecensione(r))
+                        .collect(Collectors.toList());
+        return recensioni;
+    }
+
+    private static String toString(Collection<String> c) {
+        String result =
+                c.stream()
+                        .map(n -> String.valueOf(n))
+                        .collect(Collectors.joining(",", "", ""));
+        return result;
+    }
 
 }
